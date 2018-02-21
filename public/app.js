16ecada5f9cb5f9fafc1da76d57b0d3412f82227
David Rawson <dm.rawson@gmail.com>
2018-02-21 16:34:05 +0000

    Displaying beer name list

--- public/app.js
+++ public/app.js
@@ -6,8 +6,26 @@ const app = function () {
 const makeRequest = function(url, callbackComplete){
   const request = new XMLHttpRequest();
   request.open("GET", url);
-  request.eventListener("load", callbackComplete);
+  request.addEventListener("load", callbackComplete);
   request.send();
+}
+
+const requestComplete = function(){
+  if (this.status !== 200) return;
+  const jsonString = this.responseText;
+
+  const beers = JSON.parse(jsonString);
+
+  beers.forEach(function(beer){
+    displayNames(beer);
+  })
+}
+
+const displayNames = function(beer){
+  const ul = document.getElementById("beers-list");
+  const name_li = document.createElement("li");
+  name_li.innerText = beer.name;
+  ul.appendChild(name_li);
 }
 
 
